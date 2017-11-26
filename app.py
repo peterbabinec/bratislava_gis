d1d4ed77d20a92dec423c7f1b14d2f5dc17683ed
xbabinec <babinec.peter@gmail.com>
2017-11-26 02:07:15 +0100

    Add all three main scenars

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template, request
 from database import DatabasePort
 import json
+import requests
 from psycopg2.extras import RealDictCursor, DictCursor
 from itertools import chain
 
@@ -19,38 +20,157 @@ def hello():
     return get_hello(name)
 
 
-@app.route("/api/v1.0/fitness", methods=['GET'])
-def fitness():
-    dist = request.args.get('dist')
-    lat = request.args.get('lat')
-    lon = request.args.get('lon')
-    return get_fitness(dist, lat, lon)
+@app.route("/api/v1.0/gym", methods=['GET'])
+def gym_geodata():
+    dist = int(request.args.get('dist'))
+    street_name = request.args.get('streetName')
+    street_number = request.args.get('streetNumber')
+    geocode_location = geocode_address(street_name, street_number)
+    return get_gym_geodata(dist, geocode_location)
 
 
-def get_fitness(dist, lat, lon):
+@app.route("/api/v1.0/pitchSport", methods=['GET'])
+def pitch_sport_list():
+    return get_pitch_sport_list()
+
+
+@app.route("/api/v1.0/pitch", methods=['GET'])
+def pitch_getodata():
+    sport_type = request.args.get('sportType')
+    return get_pitch_geodata(sport_type)
+
+
+@app.route("/api/v1.0/cityAreaRunning", methods=['GET'])
+def city_area_running_geodata():
+    area_name = request.args.get('areaName')
+    area_polygon = get_city_area_geodata(area_name)
+    running_lines = get_running_geodata(area_name)
+    shape_collection = area_polygon + running_lines
+    return jsonify(shape_collection)
+
+
+def get_city_area_geodata(areaName):
     dp = DatabasePort()
-    with dp.connection_handler(commit=True, cursor_factory=DictCursor) as cursor:
+    with dp.connection_handler(commit=True,
+                               cursor_factory=DictCursor) as cursor:
         query = "SELECT json_build_object(" \
                 "'type', 'Feature'," \
-                "'geometry', ST_AsGeoJSON(ST_Transform(sub.way, 4326))::json," \
+                "'geometry', ST_AsGeoJSON(ST_Transform(way, 4326))::json," \
                 "'properties', json_build_object(" \
-                "   'title', sub.name," \
-                "   'description', coalesce(sub.leisure, sub.amenity)," \
-                "   'marker-color', '#3bb2d0'," \
-                "   'marker-size', 'medium'," \
-                "   'marker-symbol', 'building'" \
-                "   )" \
-                ") FROM (" \
-                "SELECT way, name, leisure, amenity FROM planet_osm_point WHERE leisure = 'fitness_centre' OR amenity='gym' " \
-                ") AS sub;"
-        # query = "SELECT name FROM planet_osm_point WHERE leisure = 'fitness_centre';"
+                "   'title', name," \
+                "   'fill', '#003b84'," \
+                "   'fill-opacity', 0.15," \
+                "   'stroke-width', 5," \
+                "   'stroke-opacity', 0.9" \
+                "    )" \
+                ") FROM planet_osm_polygon " \
+                "WHERE name = %s"
+        cursor.execute(query, (areaName,))
+        rows = list(chain(*cursor.fetchall()))
+        return rows
+
+
+def get_running_geodata(areaName):
+    dp = DatabasePort()
+    with dp.connection_handler(commit=True,
+                               cursor_factory=DictCursor) as cursor:
+        query = "WITH running AS (" \
+                "   SELECT DISTINCT ST_Transform(line.way, 4326) AS line_way, line.highway " \
+                "   FROM planet_osm_polygon AS polygon " \
+                "   CROSS JOIN planet_osm_line AS line " \
+                "   WHERE polygon.name = %s AND ST_Intersects(polygon.way, line.way) " \
+                "   AND line.highway " \
+                "   IN ('sidewalk', 'path', 'footway', 'bridleway', 'steps', 'pedestrian', 'living_street') " \
+                "   ) " \
+                "SELECT json_build_object(" \
+                "'type', 'Feature'," \
+                "'geometry', ST_AsGeoJSON(line_way)::json," \
+                "'properties', json_build_object(" \
+                "   'title', highway," \
+                "   'description', round(ST_Length(line_way::geography)) || 'm'," \
+                "   'stroke', '#d1004c'," \
+                "   'stroke-opacity', 0.7," \
+                "   'stroke-width', 3.25" \
+                "    )" \
+                ") FROM running;"
+        cursor.execute(query, (areaName,))
+        rows = list(chain(*cursor.fetchall()))
+        return rows
+
+
+def get_pitch_geodata(sport_type):
+    dp = DatabasePort()
+    with dp.connection_handler(commit=True,
+                               cursor_factory=DictCursor) as cursor:
+        query = "SELECT json_build_object(" \
+                "'type', 'Feature'," \
+                "'geometry', ST_AsGeoJSON(ST_Transform(way, 4326))::json," \
+                "'properties', json_build_object(" \
+                "   'title', sport," \
+                "   'description', round(ST_Area(ST_Transform(way, 4326)::geography)) || ' m2'," \
+                "   'fill', '#ff1462'," \
+                "   'stroke', '#ff1462'," \
+                "   'stroke-width', 1.5" \
+                "    )" \
+                ") FROM planet_osm_polygon " \
+                "WHERE leisure = 'pitch' AND sport = %s"
+        cursor.execute(query, (sport_type,))
+        rows = list(chain(*cursor.fetchall()))
+        return jsonify(rows)
+
+
+def get_pitch_sport_list():
+    dp = DatabasePort()
+    with dp.connection_handler(commit=True) as cursor:
+        query = "SELECT DISTINCT unnest(string_to_array(plgn.sport, ';')) " \
+                "AS sport FROM planet_osm_polygon AS plgn " \
+                "WHERE leisure = 'pitch';"
         cursor.execute(query)
         rows = list(chain(*cursor.fetchall()))
         return jsonify(rows)
 
 
-
-
+def get_gym_geodata(dist, search_location):
+    lat = search_location['lat']
+    lng = search_location['lng']
+    postgis_location = 'SRID=4326;POINT(' + str(lng) + ' ' + str(lat) + ')'
+    dp = DatabasePort()
+    with dp.connection_handler(commit=True, cursor_factory=DictCursor) as cursor:
+        query = "SELECT json_build_object(" \
+                "'type', 'Feature'," \
+                "'geometry', ST_AsGeoJSON(sub.geog_way)::json," \
+                "'properties', json_build_object(" \
+                "   'title', sub.name," \
+                "   'description', coalesce(sub.leisure, sub.amenity)," \
+                "   'marker-color', '#3bb2d0'," \
+                "   'marker-size', 'medium'," \
+                "   'marker-symbol', 'circle'" \
+                "   )" \
+                ") FROM (" \
+                "SELECT ST_Transform(way, 4326) AS geog_way, name, leisure, amenity FROM planet_osm_point " \
+                "WHERE leisure = 'fitness_centre' OR amenity = 'gym' " \
+                "UNION " \
+                "SELECT ST_Transform(ST_Centroid(way), 4326) AS geog_way, name, leisure, amenity FROM planet_osm_polygon " \
+                "WHERE leisure = 'fitness_centre' OR amenity = 'gym' " \
+                ") AS sub WHERE ST_DWithin(sub.geog_way::geography, ST_GeogFromText(%s), %s);"
+        cursor.execute(query, (postgis_location, dist))
+        rows = list(chain(*cursor.fetchall()))
+        search_location_point = {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [lng, lat]
+            },
+            'properties': {
+                'title': 'Your location',
+                'description': 'Distance to search = ' + str(dist) + 'm',
+                'marker-color': '#f44274',
+                'marker-size': 'large',
+                'marker-symbol': 'star'
+            }
+        }
+        rows.append(search_location_point.copy())
+        return jsonify(features=rows)
 
 
 def get_hello(name):
@@ -59,6 +179,18 @@ def get_hello(name):
         'name': name
     }
     return jsonify(hello_message)
+
+
+def geocode_address(street_name, street_number):
+    street_name = street_name.replace(' ', '+')
+    with open('./config/geocoding.json') as geocoding:
+        geocoding_data = json.load(geocoding)
+        api_key = geocoding_data['apiKey']
+        geocode_uri = "https://maps.googleapis.com/maps/api/geocode/json?address=" + \
+              street_name + "+" + street_number + "+Bratislava&key=" + api_key
+        json_response = requests.get(geocode_uri).json()
+        location = json_response['results'][0]['geometry']['location']
+        return location
 
 
 if __name__ == "__main__":
--- static/application.js
+++ static/application.js
@@ -1,23 +1,146 @@
 var lat, lon;
+var gymLayerGroup = L.layerGroup();
+var sportList = [];
+const url = '/api/v1.0/';
 
-function searchGym() {
+
+function loadGymPoints(streetName, streetNumber, dist) {
   var request = new XMLHttpRequest();
-  request.open('GET', '/api/v1.0/fitness', true);
+  var params = "?dist=" + dist + "&streetName=" + streetName + "&streetNumber=" + streetNumber;
+  request.open('GET', url + 'gym' + params, true);
   
   request.onload = function () {
     if (request.status >= 200 && request.status < 400) {
       var gymData = JSON.parse(request.responseText);
-      console.log(gymData);
-      var myLayer = L.mapbox.featureLayer().addTo(map);
-      myLayer.setGeoJSON(gymData);
+      gymData['type'] = 'FeatureCollection';
+      var mapLayer = L.mapbox.featureLayer(gymData);
+      gymLayerGroup.clearLayers();
+      gymLayerGroup.addLayer(mapLayer);
     } else {
       alert("Load Error");
     }
   };
-  
   request.onerror = function () {
     alert("Connection Error");
   };
-  
   request.send();
+}
+
+
+function renderGymPoints() {
+  var streetNumber = document.getElementById("streetNumber").value;
+  var streetName = document.getElementById("streetName").value;
+  var dist = document.getElementById("distance").value;
+  loadGymPoints(streetName, streetNumber, dist);
+  gymLayerGroup.addTo(map);
+}
+
+
+function loadPitchSportList() {
+  var request = new XMLHttpRequest();
+  request.open('GET', url + 'pitchSport', true);
+  
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      var pitchSportData = JSON.parse(request.responseText);
+      sportList = pitchSportData.slice();
+      document.getElementById('dropdownSportList').appendChild(makeUl(sportList.slice()));
+    } else {
+      alert("Load Error");
+    }
+  };
+  request.onerror = function () {
+    alert("Connection Error");
+  };
+  request.send();
+}
+
+
+function loadPitchPolygons(sportType){
+  var request = new XMLHttpRequest();
+  var params = "?sportType=" + sportType;
+  request.open('GET', url + 'pitch' + params, true);
+  
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      var pitchData = JSON.parse(request.responseText);
+      pitchData['type'] = 'FeatureCollection';
+      var pitchLayer = L.mapbox.featureLayer(pitchData);
+      gymLayerGroup.clearLayers();
+      gymLayerGroup.addLayer(pitchLayer);
+    } else {
+      alert("Load Error");
+    }
+  };
+  request.onerror = function () {
+    alert("Connection Error");
+  };
+  request.send();
+}
+
+
+function renderPitchPolygons(){
+  var dropDownBtn = document.getElementById("dropdownButton");
+  var dropDownBtnTxt = dropDownBtn.childNodes[0];
+  loadPitchPolygons(dropDownBtnTxt.nodeValue);
+  gymLayerGroup.addTo(map);
+}
+
+
+function loadCityAreaRunningShapes(areaName){
+  var request = new XMLHttpRequest();
+  var params = "?areaName=" + areaName;
+  request.open('GET', url + 'cityAreaRunning' + params, true);
+  
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      var cityAreaData = JSON.parse(request.responseText);
+      cityAreaData['type'] = 'FeatureCollection';
+      var cityAreaLayer = L.mapbox.featureLayer(cityAreaData);
+      gymLayerGroup.clearLayers();
+      gymLayerGroup.addLayer(cityAreaLayer);
+    } else {
+      alert("Load Error");
+    }
+  };
+  request.onerror = function () {
+    alert("Connection Error");
+  };
+  request.send();
+}
+
+
+function renderCityAreaRunningShapes() {
+  var areaName = document.getElementById('cityArea').value;
+  loadCityAreaRunningShapes(areaName);
+  gymLayerGroup.addTo(map);
+}
+
+
+function initSliderRange() {
+  var slider = document.getElementById("distance");
+  var output = document.getElementById("demo");
+  output.innerHTML = slider.value;
+  slider.oninput = function () {
+    output.innerHTML = this.value;
+  };
+}
+
+
+function makeUl(array) {
+  var list = document.createElement('ul');
+  list.className = "dropdown-menu";
+  for (var i = 0; i < array.length; i++) {
+    var item = document.createElement('li');
+    item.appendChild(document.createTextNode(array[i]));
+    item.setAttribute('id', 'list' + i);
+    item.setAttribute('class', 'sport-li');
+    item.onclick = function () {
+      var dropdownButton = document.getElementById("dropdownButton");
+      var dropdownBtnText = dropdownButton.childNodes[0];
+      dropdownBtnText.nodeValue = this.innerHTML;
+    };
+    list.appendChild(item);
+  }
+  return list;
 }
